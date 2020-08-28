a1ceb9f7df6960c020408b4d044af563d40604bb
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-08-28 19:51:02 +0200

    Création du projet TestOgreBoulotteur

---
+++ TestOgreBoulotteur/TestOgreBoulotteur.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestOgreBoulotteur
+{
+	TEST_CLASS(TestOgreBoulotteur)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
