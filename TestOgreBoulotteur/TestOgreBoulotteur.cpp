c4fecd3696dfdc3d8fcaba6c9929554310720fae
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-05 00:20:22 +0200

    Add 2 new tests

--- TestOgreBoulotteur/TestOgreBoulotteur.cpp
+++ TestOgreBoulotteur/TestOgreBoulotteur.cpp
@@ -21,12 +21,26 @@ public:
         for (y = 0; y < FORET_HAUTEUR; y++) {
             for (x = 0; x < FORET_LONGUEUR; x++) {
                 if ((y == 0) || (y == (FORET_HAUTEUR - 1)) || (x == 0) || (x == (FORET_LONGUEUR - 1))) {
-                    Assert::AreEqual ((char)FORET_SOL, foret[y][x]);
+                    Assert::AreEqual ((char)FORET_ARBRE, foret[y][x]);
                 } else {
                     Assert::IsTrue (((char)FORET_SOL == foret[y][x]) || ((char)FORET_ARBRE == foret[y][x]));
                 }
             }
         }
     }
+
+    TEST_METHOD (f_initialiserOgre) {
+        Foret foret;
+        Ogre ogre;
+
+        initialiserForet (foret);
+        initialiserOgre (&ogre, foret);
+        Assert::IsTrue (ogre.x > 0);
+        Assert::IsTrue (ogre.y > 0);
+        Assert::IsTrue (ogre.x < FORET_LONGUEUR);
+        Assert::IsTrue (ogre.y < FORET_HAUTEUR);
+        Assert::IsTrue ((char)FORET_SOL == foret[ogre.y][ogre.x]);
+    }
+
     };
 }
